c5d0c884ce98eece320372bfe9c27b486d4d56ab
stefanelli990 <stefan.mohenski@gmail.com>
2022-01-16 03:10:51 +0100

    last change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './styles/css/styles.css';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/it-company">
         <Navbar/>
         <Routes>
           <Route path="/" exact element={<Home/>} />
